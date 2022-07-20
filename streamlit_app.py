52040f04c295880b9902ed9a7486ce21f8c9eca1
AndrejMohoric <32498663+AndrejMohoric@users.noreply.github.com>
2022-07-20 13:48:15 +0200

    Update streamlit_app.py
    Addet emogies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('Test Nalov')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍌Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥭Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥝Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
