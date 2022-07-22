2039aea4121217c792d2f653a1f972b89c014b58
AndrejMohoric <32498663+AndrejMohoric@users.noreply.github.com>
2022-07-22 15:43:10 +0200

    Kivi querry

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit
 import pandas 
 import requests
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,3 +18,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon/"+ kiwi")
+# write your own comment -what does the next line do? 
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
